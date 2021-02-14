6f4ca796f3713f9a4d6bb52f1a0a3e1ec8ec1cdb
barisbayin@gmail.com <barisbayin@gmail.com>
2021-02-14 10:20:27 +0300

    Study_14.02.2021

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            //Burada sanki veritabanından veri geliyormuş gibi simüle ediyoruz.
+            _cars = new List<Car>
+            {
+                new Car{Id = 1, ColorId = 1, BrandId =1, ModelYear =2020, DailyPrice = 150, Description = "Dizel araç"},
+                new Car{Id = 2, ColorId = 2, BrandId =2, ModelYear =2021, DailyPrice = 200, Description = "Benzinli araç"},
+                new Car{Id = 3, ColorId = 3, BrandId =3, ModelYear =2021, DailyPrice = 300, Description = "Elektrikli araç"},
+                new Car{Id = 4, ColorId = 1, BrandId =1, ModelYear =2020, DailyPrice = 400, Description = "Hibrit araç"}
+            };
+        }
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(p => p.Id == id).ToList();
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            // Gönderdiğimiz ürünün ID2sine sahip ürünü bul.
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+
+        }
+
+        public void Delete(Car car)
+        {
+            //Eğer Linq kullanmasaydık buradaki kodlarla bir döngü yaratıp bizim gönderdiğimiz car ID'sini listemizde(ya da vertabanımızda) bulunan car ID'si ile karşılaştırcaktık. Eşleşmeyi bulduktan sonra da car nesnesini "_cars.Remove(carToDelete);" fonksiyonu ile silecektik.
+
+            //Car carToDelete=null;
+            //foreach (var p in _cars )
+            //{
+            //    if (car.Id == p.Id)
+            //    {
+            //        carToDelete = p;
+            //    }
+            //}
+
+
+            Car carToDelete = _cars.SingleOrDefault(p => p.Id == car.Id);
+
+            _cars.Remove(carToDelete);
+
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden bir veri tabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
